82022e387f9679e9823dd2fa17dc820f5ea8d339
smthri-home <lockshin1999@mail.ru>
2022-03-12 23:02:33 +0300

    opencv

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 extern "C" void fill_value(int* res);
 
-int main() {
-    std::cout << "Input number" << std::endl;
-    int x;
-    std::cin >> x;
-    int res = x;
-    fill_value(&res);
-    std::cout << x << " * 100 = " << res << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Please specify input / output images" << std::endl;
+    }
+
+    std::string fname_in(argv[1]);
+    cv::Mat src_img = cv::imread(fname_in, cv::IMREAD_COLOR);
+    cv::imwrite("out.png", src_img);
     return 0;
 }
